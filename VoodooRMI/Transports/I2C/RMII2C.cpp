a95bf9419422554a144a1d2ccab6c3c1cb7cd423
zhen-zen <66577170+zhen-zen@users.noreply.github.com>
2020-08-21 23:20:14 -0700

    Identify SYNA trackpads with VendorID

--- VoodooRMI/Transports/I2C/RMII2C.cpp
+++ VoodooRMI/Transports/I2C/RMII2C.cpp
@@ -46,15 +46,13 @@ RMII2C *RMII2C::probe(IOService *provider, SInt32 *score) {
         acpi_device->retain();
         // Sometimes an I2C HID will have power state methods, lets turn it on in case
         acpi_device->evaluateObject("_PS0");
-        if (getHIDDescriptorAddress() != kIOReturnSuccess)
-            IOLog("%s::%s Could not get HID descriptor address\n", getName(), name);
     }
 
-    if (getHIDDescriptor() != kIOReturnSuccess) {
-        hdesc.wCommandRegister  = RMI_HID_COMMAND_REGISTER;
-        hdesc.wDataRegister     = RMI_HID_DATA_REGISTER;
-        hdesc.wOutputRegister   = RMI_HID_OUTPUT_REGISTER;
-        IOLog("%s::%s Using default HID register addresses\n", getName(), name);
+    if (getHIDDescriptorAddress() != kIOReturnSuccess ||
+        getHIDDescriptor() != kIOReturnSuccess ||
+        hdesc.wVendorID != 0x6cb) {
+        IOLog("%s::%s Could not get valid HID descriptor\n", getName(), name);
+        return NULL;
     }
 
     do {

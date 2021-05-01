66363a797729972f67013239bc9e20e357c007d4
zhen-zen <66577170+zhen-zen@users.noreply.github.com>
2021-05-01 12:14:04 -0700

    Invert dy

--- VoodooRMI/Functions/F17.cpp
+++ VoodooRMI/Functions/F17.cpp
@@ -246,7 +246,7 @@ int F17::rmi_f17_process_stick(struct rmi_f17_stick_data *stick) {
                 clock_get_uptime(&timestamp);
                 
                 relativeEvent.dx = (SInt32)((SInt64)stick->data.rel.x_delta * conf->trackpointMult / DEFAULT_MULT);
-                relativeEvent.dy = (SInt32)((SInt64)stick->data.rel.y_delta * conf->trackpointMult / DEFAULT_MULT);
+                relativeEvent.dy = -(SInt32)((SInt64)stick->data.rel.y_delta * conf->trackpointMult / DEFAULT_MULT);
                 relativeEvent.buttons = 0;
                 relativeEvent.timestamp = timestamp;
                 
